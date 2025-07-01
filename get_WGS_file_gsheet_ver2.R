184a6cb8f39478e5f22483f31c846d5decb1120a
amsbarnuevo <allysa.barnuevo@ritm.gov.ph>
2025-07-01 15:53:22 +0800

    Update script

--- get_WGS_file_gsheet_ver2.R
+++ get_WGS_file_gsheet_ver2.R
@@ -24,9 +24,10 @@ sheet_id <- "1rOFp9VHXeAfghFU6aOOBeBSt9Q1RC5WVAdLez4HHLU4"
 # Read the sheet
 df_batch <- read_sheet(sheet_id, sheet = "batch")
 df_qualifyr <- read_sheet(sheet_id, sheet = "qualifyr")
+df_gambit <- read_sheet(sheet_id, sheet = "gambit")
 df_bactopia <- read_sheet(sheet_id, sheet = "bactopia-report")
+df_assembly <- read_sheet(sheet_id, sheet = "assembly-scan")
 df_checkm2 <- read_sheet(sheet_id, sheet = "checkm2")
-#df_mlst <- read_sheet(sheet_id, sheet = "mlst", col_names = FALSE)
 df_mlst <- read_sheet(sheet_id, sheet = "mlst")
 df_amrfinderplus <- read_sheet(sheet_id, sheet = "amrfinderplus")
 
@@ -45,12 +46,12 @@ sample_name <- gsub("-", "_", sample_name, fixed=TRUE)
 sample_name_clean <- sub("(_[0-9]+.*)$", "", sample_name)
 
 
-#merge qualifyr, checkm2 and bactopia results
+#merge qualifyr, gambit,checkm2 and bactopia results
 #rename column
-colnames(df_qualifyr)[which(names(df_qualifyr) == "bactinspector.species.metric_value")] <- "species"
-colnames(df_qualifyr)[which(names(df_qualifyr) == "sample_name")] <- "name"
+colnames(df_gambit)[which(names(df_gambit) == "query")] <- "name"
+colnames(df_assembly)[which(names(df_assembly) == "sample")] <- "name"
 colnames(df_bactopia)[which(names(df_bactopia) == "sample")] <- "name"
-colnames(df_bactopia)[which(names(df_bactopia) == "species")] <- "species_name"
+
 
 #checkm2 column names to lower
 names(df_checkm2) <- tolower(names(df_checkm2))
@@ -58,14 +59,28 @@ names(df_checkm2) <- tolower(names(df_checkm2))
 #remove ".fna"
 df_checkm2$name <- gsub(".fna", "", df_checkm2$name, fixed=TRUE)
 
-#merge df
-quality_report <- merge(df_qualifyr, df_checkm2, by = "name")
-quality_report <- merge(quality_report, df_bactopia, by = "name")
+
+# Get species name
+df_gambit$species <- ifelse(df_gambit$predicted.rank == "species", df_gambit$predicted.name, df_gambit$next.name)
+
+# Compute total GC Content
+df_assembly$gc_content <- as.numeric(df_assembly$contig_percent_g) + as.numeric(df_assembly$contig_percent_c) 
 
 
-wgs_df <- subset(quality_report, select = c(name, species, completeness, contamination, rank, qc_final_coverage,
-                                            assembler_total_contig_length, gc_content , assembler_total_contig,
-                                            assembler_n50_contig_length, qc_final_qual_mean))
+
+# Subset Columns per dataframe
+gambit_sub_df <- subset(df_gambit, select = c(name, species))
+checkm2_sub_df <- subset(df_checkm2, select = c(name, completeness, contamination))
+assembly_sub_df <- subset(df_assembly, select = c(name, total_contig, total_contig_length, gc_content, n50_contig_length))
+bactopia_sub_df <- subset(df_bactopia, select = c(name, qc_original_total_bp, qc_final_qual_mean))
+
+
+
+# put all data frames into list
+df_list <- list(gambit_sub_df, checkm2_sub_df, assembly_sub_df, bactopia_sub_df)
+
+# merge all data frames in list
+wgs_df <- df_list %>% reduce(full_join, by='name')
 
 
 wgs_df$name <- gsub("-", "_", wgs_df$name, fixed=TRUE)
@@ -165,6 +180,22 @@ if(bbr_sample_count !=0){
     add_row(sample_id = sample_id, arsrl_org=arsrl_org)
   
 }
+
+
+#Check if QC_BBR sample is present in the id list
+vc_sample <- grep("VC", wgs_df[['sample_id']], value = TRUE)
+vc_sample_count <- length(vc_sample)
+
+#manually add result for QC_BBR
+if(vc_sample_count !=0){
+  sample_id = vc_sample
+  arsrl_org = "Neisseria gonorrhoeae"
+  
+  arsrl_result_df <- arsrl_result_df %>% 
+    add_row(sample_id = sample_id, arsrl_org=arsrl_org)
+  
+}
+
 
 
 wgs_df1 <- wgs_df
